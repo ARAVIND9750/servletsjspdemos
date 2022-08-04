9e19f94ee3525face64daf01fe6a74a183221c6f
aravind <aravindreigns9750@gmail.com>
2022-08-04 13:24:27 +0530

    Changes have been committed

---
+++ S01HelloWorld/src/main/java/javax/servlet/GenericServlet.java
@@ -0,0 +1,12 @@
+package javax.servlet;
+
+import java.io.IOException;
+
+public class GenericServlet {
+
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
